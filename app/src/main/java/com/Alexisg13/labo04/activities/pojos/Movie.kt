b0241768c00fc5f3f8584fa131ee3025fd0f3533
Alexis Gómez <Alexis Gmez>
2019-04-05 15:41:47 -0600

    Se añadio labo

---
+++ app/src/main/java/com/Alexisg13/labo04/activities/pojos/Movie.kt
@@ -0,0 +1,18 @@
+package com.Alexisg13.labo04.activities.pojos
+
+import android.os.Parcel
+import android.os.Parcelable
+
+data class Movie(
+        val Title:String = "N/A",
+        val Year:String="N/A",
+        val Released:String="N/A",
+        val Runtime:String="N/A",
+        val Genre:String="N/A",
+        val Director:String="N/A",
+        val Actors:String="N/A",
+        val Plot:String="N/A",
+        val Language:String="N/A",
+        val imdbRating:String="N/A",
+        val Poster:String="N/A"
+        )
